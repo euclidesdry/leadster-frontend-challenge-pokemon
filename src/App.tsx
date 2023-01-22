3615fbdde73e6938b8be932073ab2f130ab5a9f8
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-22 18:10:42 +0100

    feat(project): ✨ adding app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import AppRoutes from './routes/router';
 
 export default function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Meu app</p>
-      </header>
+      <header className='App-header' />
+      <AppRoutes />
     </div>
   );
 }
