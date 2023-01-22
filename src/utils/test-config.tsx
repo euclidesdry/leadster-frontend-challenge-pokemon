12ae00e5eec381c4e1d277957675211b166e9bf8
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-22 20:25:00 +0100

    fix(lint): 🚨 test utils duplicated exports

--- src/utils/test-config.tsx
+++ src/utils/test-config.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
-import { render, RenderOptions } from '@testing-library/react';
+// eslint-disable-next-line import/named
+import { RenderOptions, render } from '@testing-library/react';
 
 function AllTheProviders({ children }: { children: React.ReactNode }) {
   // eslint-disable-next-line react/jsx-no-useless-fragment
@@ -9,5 +10,7 @@ function AllTheProviders({ children }: { children: React.ReactNode }) {
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
+// eslint-disable-next-line import/export
 export * from '@testing-library/react';
+// eslint-disable-next-line import/export
 export { customRender as render };
