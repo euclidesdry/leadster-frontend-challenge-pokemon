6086983c93d989d756c0a562e6e40a68c68a9ca6
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-24 23:56:42 +0100

    feat(component): ✨ adding DarkModeSwitcher component

---
+++ src/components/DarkModeSwitcher/DarkModeSwitcher.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as Switch from '@radix-ui/react-switch';
+import { Label, Switcher } from './styles';
+import { useAppContext } from '../../contexts/App';
+
+export default function DarkModeSwitcher() {
+  const { settings, toggleDarkMode } = useAppContext();
+
+  const labelText = settings.darkMode ? 'Dark mode' : 'Light mode';
+
+  return (
+    <Switcher data-testid='DarkModeSwitcher-component'>
+      <Label htmlFor='dark-mode'>{labelText}</Label>
+      <Switch.Root className='SwitchRoot' id='dark-mode' checked={settings.darkMode} onClick={toggleDarkMode}>
+        <Switch.Thumb className='SwitchThumb' />
+      </Switch.Root>
+    </Switcher>
+  );
+}
