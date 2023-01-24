ed46650c98ae2cd3b839585288882cd1a6d7803b
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-25 00:32:42 +0100

    feat(styles): 💄 adding Header component styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,4 +2,25 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
   height: 104px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LogoBox = styled.span`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+export const Logo = styled.img`
+  width: 48px;
+  height: 48px;
+  transform: rotate(45deg);
+`;
+
+export const LogoText = styled.strong`
+  display: inline-block;
+  font-size: 1.25rem;
 `;
