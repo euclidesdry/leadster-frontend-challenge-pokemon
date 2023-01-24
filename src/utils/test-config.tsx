590c0a8bbe07c410a953cf087dfc978841f41f9b
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-24 23:43:49 +0100

    feat: ✨ adding AppProvider on the test utils

--- src/utils/test-config.tsx
+++ src/utils/test-config.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from 'react';
 // eslint-disable-next-line import/named
 import { RenderOptions, render } from '@testing-library/react';
+import AppProvider from '../contexts';
 
 function AllTheProviders({ children }: { children: React.ReactNode }) {
   // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{children}</>;
+  return <AppProvider>{children}</AppProvider>;
 }
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
