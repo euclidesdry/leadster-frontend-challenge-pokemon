d5d9f6fd847b83ec3b231e710cf0c12a93fce18b
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-24 23:58:24 +0100

    test(component): 🧪 adding DarkModeSwitcher tests suite

--- src/components/DarkModeSwitcher/DarkModeSwitcher.test.tsx
+++ src/components/DarkModeSwitcher/DarkModeSwitcher.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DarkModeSwitcher from './DarkModeSwitcher';
-import { render, screen } from '../../utils/test-config';
+import { fireEvent, render, screen } from '../../utils/test-config';
 
 describe('Switch component', () => {
   test('should be rendered correctly', () => {
@@ -8,5 +8,19 @@ describe('Switch component', () => {
 
     const darkModeSwitcherComponent = screen.getByTestId('DarkModeSwitcher-component');
     expect(darkModeSwitcherComponent).toBeTruthy();
+  });
+
+  test('should checked state be toggled when user clicks to switch', async () => {
+    render(<DarkModeSwitcher />);
+
+    const switchButton = screen.getByRole('switch');
+
+    expect(screen.getByLabelText(/Light mode/)).toBeTruthy();
+
+    fireEvent.click(switchButton);
+    expect(screen.getByLabelText(/Dark mode/)).toBeTruthy();
+
+    fireEvent.click(switchButton);
+    expect(screen.getByLabelText(/Light mode/)).toBeTruthy();
   });
 });
