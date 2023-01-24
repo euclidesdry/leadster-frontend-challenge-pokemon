72faf982df568d7e5fb6c7c89735c3f0c7f86dd5
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-25 00:35:43 +0100

    test(component): ✅ adding inside components check test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,8 +3,21 @@ import Header from './Header';
 import { render, screen } from '../../../src/utils/test-config';
 
 describe('Header component', () => {
-  test('should be rendered correctly', () => {
+  test('should be rendered correctly', async () => {
     render(<Header />);
     expect(screen.getByTestId('Header-component')).toBeTruthy();
+  });
+
+  test('should have header components inside', async () => {
+    render(<Header />);
+
+    const logoImage = screen.getByAltText('app-logo');
+    expect(logoImage).toBeTruthy();
+
+    const appName = screen.getByText('PokeApp');
+    expect(appName).toBeTruthy();
+
+    const themeSwitcher = screen.getByTestId('DarkModeSwitcher-component');
+    expect(themeSwitcher).toBeTruthy();
   });
 });
