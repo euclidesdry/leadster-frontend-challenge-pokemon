f5e08c2e479cb3731999cef28981573928bd93c8
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-25 00:34:37 +0100

    feat(component): ✨ adding Header Logo & theme Switcher
    Closes #6

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { HeaderContainer } from './styles';
+import { HeaderContainer, Logo, LogoBox, LogoText } from './styles';
+
+import appLogo from '../../assets/img/logo.svg';
+import DarkModeSwitcher from '../DarkModeSwitcher';
 
 export default function Header() {
   return (
     <HeaderContainer className='app-container' data-testid='Header-component'>
-      Header
+      <LogoBox>
+        <Logo src={appLogo} alt='app-logo' />
+        <LogoText>PokeApp</LogoText>
+      </LogoBox>
+
+      <DarkModeSwitcher />
     </HeaderContainer>
   );
 }
