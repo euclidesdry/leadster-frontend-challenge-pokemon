cfbfcafb7d30bd841df197cefe3e228783e031bd
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-25 00:00:11 +0100

    feat(styles): 💄 adding DarkModeSwitcher component styles
    Closes #6

---
+++ src/components/DarkModeSwitcher/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Switcher = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const Label = styled.label`
+  /* color: white; */
+  font-size: 15px;
+  line-height: 1;
+  margin-right: 15px;
+`;
