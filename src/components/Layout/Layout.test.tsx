a17ad23edbaf592cbdcda8f842ed1fcf145ac631
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-24 18:06:57 +0100

    test(component): ✅ testing children node component inside

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { render, screen } from '../../utils/test-config';
 import Layout from './Layout';
 
+const MOCK_EXPECTED_TEXT = 'Testing';
+
 describe('Layout component', () => {
   test('should be rendered correctly', () => {
     render(<Layout />);
     expect(screen.getByTestId('Layout-component')).toBeTruthy();
+  });
+
+  test('should have a child component inside', () => {
+    render(
+      <Layout>
+        <p>{MOCK_EXPECTED_TEXT}</p>
+      </Layout>
+    );
+    expect(screen.getByText(MOCK_EXPECTED_TEXT)).toBeTruthy();
   });
 });
