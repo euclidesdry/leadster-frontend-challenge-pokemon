27ae768917fb1230c116cb2299eea40270d4db02
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-24 23:47:15 +0100

    feat(app): ✨ adding dark theme auto setup by app context change
    Closes #6

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import AppRoutes from './routes/router';
 import Header from './components/Header';
 
 import './assets/styles/global.scss';
+import { useAppContext } from './contexts/App';
 
 export default function App() {
+  const { settings } = useAppContext();
+  const appThemeMode = settings.darkMode ? 'dark' : 'light';
+
   return (
-    <div className='app' data-testid='App'>
+    <div className='app' data-testid='App' data-theme={appThemeMode}>
       <Header />
       <AppRoutes />
     </div>
