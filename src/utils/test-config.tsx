23063ad400f90525b9bd75b5adc9f72e9b4b309e
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-25 11:04:57 +0100

    feat(tests): ✅ adding query client provider on test utils

--- src/utils/test-config.tsx
+++ src/utils/test-config.tsx
@@ -2,10 +2,17 @@ import React, { ReactElement } from 'react';
 // eslint-disable-next-line import/named
 import { RenderOptions, render } from '@testing-library/react';
 import AppProvider from '../contexts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function AllTheProviders({ children }: { children: React.ReactNode }) {
   // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <AppProvider>{children}</AppProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>{children}</AppProvider>;
+    </QueryClientProvider>
+  );
 }
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
