c35c166d76e40d5e3346e8cfb445269c9d253dad
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-26 02:00:36 +0100

    feat(app): ✨ adding query provider and query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import AppRoutes from './routes/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './assets/styles/global.scss';
 import { useAppContext } from './contexts/App';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const { settings } = useAppContext();
@@ -10,7 +14,10 @@ export default function App() {
 
   return (
     <div className='app' data-testid='App' data-theme={appThemeMode}>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
