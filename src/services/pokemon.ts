84a79515c76d23881b40f23361acd02fb2ace575
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-26 01:51:17 +0100

    feat(api): ✨ adding api services listPokemons & listPokemonInfoById

---
+++ src/services/pokemon.ts
@@ -0,0 +1,12 @@
+import { ListPokemonsType, PokemonDetailsType } from '../types/services/pokemon';
+import { pokeApi } from './config';
+
+export async function listPokemons(limit = 9, offset = 0): Promise<ListPokemonsType> {
+  const response = await pokeApi.get<ListPokemonsType>(`/pokemon?limit=${limit}&offset=${offset}`);
+  return response.data;
+}
+
+export async function listPokemonInfoById(pokemonId: number): Promise<PokemonDetailsType> {
+  const response = await pokeApi.get<PokemonDetailsType>(`/pokemon/${pokemonId}`);
+  return response.data;
+}
