96c66768357a84942a8d9f049928e59659373339
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-27 02:23:28 +0100

    feat(styles): 💄 adding PokemonList component style

---
+++ src/components/PokemonList/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+
+  grid-template-columns: calc(33.333% - 14px) calc(33.333% - 14px) calc(33.333% - 14px);
+  grid-template-rows: 180px 180px 180px;
+  column-gap: 21px;
+  row-gap: 20px;
+
+  @media (max-width: 920px) {
+    grid-template-columns: calc(50% - 10px) calc(50% - 10px);
+    column-gap: 20px;
+  }
+
+  @media (max-width: 640px) {
+    grid-template-columns: calc(100%);
+    column-gap: 0;
+    grid-template
+  }
+`;
