f51c22cfb21f4b481143bd5571c7948fdf8c1447
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-27 02:20:06 +0100

    feat(component): ✨ adding navigation to home page on the app logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,28 @@ import { HeaderContainer, Logo, LogoBox, LogoText } from './styles';
 
 import appLogo from '../../assets/img/logo.svg';
 import DarkModeSwitcher from '../DarkModeSwitcher';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <HeaderContainer className='app-container' data-testid='Header-component'>
-      <LogoBox>
+      <LogoBox
+        onClick={() => {
+          navigate('/');
+        }}
+      >
         <Logo src={appLogo} alt='app-logo' />
         <LogoText>PokeApp</LogoText>
       </LogoBox>
+
+      {/* <nav>
+        <ul>
+          <li>Página Inicial</li>
+          <li>Sobre</li>
+        </ul>
+      </nav> */}
 
       <DarkModeSwitcher />
     </HeaderContainer>
