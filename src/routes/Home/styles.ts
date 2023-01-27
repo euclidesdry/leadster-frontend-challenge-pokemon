efe26bdbe84dde2d247674d26a8ff296e3562c61
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-27 02:27:01 +0100

    refactor(page): 🔥 removing unused styles from Home styles

--- src/routes/Home/styles.ts
+++ src/routes/Home/styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-const SEARCH_BAR_WIDTH = 48;
-
 type ContainerProps = {
   isDark?: boolean;
 };
@@ -21,78 +19,5 @@ export const Container = styled.div<ContainerProps>`
 
   @media (max-width: 640px) {
     padding: 20px 20px 0;
-  }
-`;
-
-export const SearchBox = styled.div`
-  display: flex;
-  width: 100%;
-  height: ${SEARCH_BAR_WIDTH}px;
-  background-color: #f5f5f5;
-  border-radius: 16px;
-  margin-bottom: ${SEARCH_BAR_WIDTH}px;
-
-  overflow: hidden;
-
-  &[data-theme='dark'] {
-    border: 1px solid black;
-  }
-`;
-
-export const Icon = styled.span`
-  min-width: ${SEARCH_BAR_WIDTH}px;
-  height: ${SEARCH_BAR_WIDTH}px;
-  padding: 0 5px 0 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #b3b3b3;
-`;
-
-export const TextInput = styled.input`
-  flex: 1;
-  border: 0 solid transparent;
-  background-color: transparent;
-  outline: none;
-`;
-
-export const SearchButton = styled.button`
-  width: 148px;
-  background-color: #262626;
-  color: white;
-  text-align: center;
-  border-radius: 0 16px 16px 0;
-  cursor: pointer;
-
-  transition: all 0.3s ease;
-
-  &:hover {
-    width: 158px;
-    background-color: #f53660;
-  }
-
-  &:active {
-    width: 148px;
-    background-color: #262626;
-  }
-`;
-
-export const PokemonList = styled.div`
-  display: grid;
-
-  grid-template-columns: calc(33.333% - 14px) calc(33.333% - 14px) calc(33.333% - 14px);
-  grid-template-rows: 180px 180px 180px;
-  column-gap: 21px;
-  row-gap: 20px;
-
-  @media (max-width: 920px) {
-    grid-template-columns: calc(50% - 10px) calc(50% - 10px);
-    column-gap: 20px;
-  }
-
-  @media (max-width: 640px) {
-    grid-template-columns: calc(100%);
-    column-gap: 0;
-    grid-template
   }
 `;
