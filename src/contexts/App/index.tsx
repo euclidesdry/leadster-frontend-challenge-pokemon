2a1a5620c403ecb11748be30b448d64cb1407349
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-27 10:31:11 +0100

    feat: ✨ adding searchTerm and page offset on the app context

--- src/contexts/App/index.tsx
+++ src/contexts/App/index.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable sort-keys */
 import * as React from 'react';
-import { AppContextType, AppType } from '../../types/app.ctx';
+import { AppContextType, AppPokemonType, AppSettingsType } from '../../types/app.ctx';
 
 export const AppContext = React.createContext<AppContextType | null>(null);
 
 type AppProviderProps = React.PropsWithChildren;
 
 export default function AppProvider({ children }: AppProviderProps) {
-  const [settings, setSettings] = React.useState<AppType>({
+  const [settings, setSettings] = React.useState<AppSettingsType>({
     darkMode: false,
+  });
+
+  const [pokemon, setPokemon] = React.useState<AppPokemonType>({
+    selected: null,
+    searchTerm: '',
+    offset: 0,
   });
 
   const toggleDarkMode = React.useCallback(() => {
@@ -17,12 +24,37 @@ export default function AppProvider({ children }: AppProviderProps) {
     }));
   }, [settings.darkMode]);
 
+  const setSelectedPokemon = React.useCallback((id: number | null) => {
+    setPokemon((oldPokemon) => ({
+      ...oldPokemon,
+      selected: id,
+    }));
+  }, []);
+
+  const setSearchTerm = React.useCallback((text: string) => {
+    setPokemon((oldTerm) => ({
+      ...oldTerm,
+      searchTerm: text,
+    }));
+  }, []);
+
+  const setOffset = React.useCallback((number: number) => {
+    setPokemon((oldPokemon) => ({
+      ...oldPokemon,
+      offset: number,
+    }));
+  }, []);
+
   const values = React.useMemo<AppContextType>(
     () => ({
+      pokemon,
       settings,
       toggleDarkMode,
+      setSelectedPokemon,
+      setSearchTerm,
+      setOffset,
     }),
-    [settings, toggleDarkMode]
+    [pokemon, setOffset, setSearchTerm, setSelectedPokemon, settings, toggleDarkMode]
   );
 
   return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
